cb025d94d0fbba162a5e0606df1a512c19871c70
rrs <enemy.forest.brigade@gmail.com>
2019-10-26 00:47:21 +0000

    switch to preact insead of react

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+    },
   },
   output: {
     filename: 'bundle.js',
