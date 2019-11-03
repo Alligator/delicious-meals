d73ce1e0dc1ef1a0e07aac081b240e26c13f23a2
rrs <enemy.forest.brigade@gmail.com>
2019-11-03 01:07:19 +0000

    Added a view that extracts the timestamp from discord snowflakes.

--- server/create-database.sql
+++ server/create-database.sql
@@ -13,3 +13,11 @@ CREATE TABLE IF NOT EXISTS votes(
   userAgent TEXT,
   timestamp TEXT DEFAULT (datetime('now', 'utc'))
 );
+
+CREATE VIEW IF NOT EXISTS messages_with_dates AS
+SELECT
+  *
+  -- discord snowflakes are 63 bit ints, the top 41 bits are the timestamp
+  -- 1420070400000 = 2015-1-1 00:00:00, the epoch for discord timestamps
+, datetime(((id >> 22) + 1420070400000) / 1000, 'unixepoch') AS date
+FROM messages;
